277a597619591301c83469120e56d6cdcdac444f
zuizihad <ahmedxihad@gmail.com>
2021-03-20 11:58:51 +0600

    initial create

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAWNl8ePt49GWgjittZnDqzomoQKBqDJmo",
+    authDomain: "react-metro.firebaseapp.com",
+    projectId: "react-metro",
+    storageBucket: "react-metro.appspot.com",
+    messagingSenderId: "390745782566",
+    appId: "1:390745782566:web:9a2e99a35a527dd970e9e7"
+};
+
+export default firebaseConfig;
